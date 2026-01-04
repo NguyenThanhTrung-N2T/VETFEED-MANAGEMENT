0edc747b1fb4d68ca8dfed07b7f82c8e2a8ed29a
ViciousEve <146147992+ViciousEve@users.noreply.github.com>
2026-01-04 17:48:50 +0700

    rework NhaCungCapDetailedResponse structure

--- VETFEED.Backend.API/DTOs/NhaCungCap/NhaCungCapDetailedResponse.cs
+++ VETFEED.Backend.API/DTOs/NhaCungCap/NhaCungCapDetailedResponse.cs
@@ -2,8 +2,17 @@ using VETFEED.Backend.API.DTOs.NhaCungCapSanPham;
 
 namespace VETFEED.Backend.API.DTOs.NhaCungCap
 {
-    public class NhaCungCapDetailedResponse : NhaCungCapResponse
+    public class NhaCungCapDetailedResponse
     {
+        public Guid MaNCC { get; set; }
+        public string? MaNCCCode { get; set; }
+        public string? TenNCC { get; set; }
+        public string? SoDienThoai { get; set; }
+        public string? DiaChi { get; set; }
+        public string? TrangThai { get; set; }
+        public string? GhiChu { get; set; }
+        public DateTime NgayTao { get; set; }
+
         public List<NhaCungCapSanPhamResponse>? SanPhams { get; set; }
     }
 }
